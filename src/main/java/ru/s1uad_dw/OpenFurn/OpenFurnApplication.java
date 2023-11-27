409ef888f4e7c60927a6f7f1587d33e8fa9df7d0
s1uad-dw <s1uad.dw@gmail.com>
2023-11-27 10:33:13 +0300

    add project files

---
+++ src/main/java/ru/s1uad_dw/OpenFurn/OpenFurnApplication.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.OpenFurn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/s1uad_dw/OpenFurn/OpenFurnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.OpenFurn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
